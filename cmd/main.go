f0aa50117bf0867b8c5814fdf891f5186bbb67c5
FrenkenFlores <saifualdin.evloev@gmail.com>
2025-01-04 16:29:45 +0300

    update #6: add configs
    Signed-off-by: FrenkenFlores <saifualdin.evloev@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,23 @@ import (
 	"github.com/FrenkenFlores/todo_go/pkg/handler"
 	"github.com/FrenkenFlores/todo_go/pkg/repository"
 	"github.com/FrenkenFlores/todo_go/pkg/service"
+	"github.com/spf13/viper"
 )
 
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
+
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error occured durring initialization: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
-	port := "8001"
+	port := viper.GetString("port")
 	srv := new(todogo.Server)
 	if err := srv.Run(port, handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running server: %s", err.Error())
